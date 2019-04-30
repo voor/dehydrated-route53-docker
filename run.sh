57ca5f006303b9e11d8510e3460361b738b1bb0b
Robert Van Voorhees <rcvanvo@gmail.com>
2019-04-30 13:23:37 -0400

    Different container name.

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ mkdir -p accounts certs
 
 touch domains.txt
 
-docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -u $UID:$GID -v $PWD/domains.txt:/home/dehydrated/domains.txt -v $PWD/certs/:/certs/ -v $PWD/accounts/:/accounts/ voor/dehydrated-route53-docker:latest
+docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -u $UID:$GID -v $PWD/domains.txt:/home/dehydrated/domains.txt -v $PWD/certs/:/certs/ -v $PWD/accounts/:/accounts/ voor/dehydrated-route53:latest
